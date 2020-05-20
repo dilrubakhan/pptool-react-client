a0ebc481796b3489839985414d763195c03282a5
Dilruba Khan <rubatanisa@yahoo.ie>
2020-05-20 23:48:19 +0100

    Implement delete project

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,10 +1,14 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
     const res = await axios.post("http://localhost:8080/api/project", project);
     history.push("/dashboard");
+    dispatch({
+      type: GET_ERRORS,
+      payload: {}
+    });
   } catch (err) {
     dispatch({
       type: GET_ERRORS,
@@ -20,8 +24,9 @@ export const getProjects = () => async dispatch => {
     payload: res.data
   });
 };
+
 export const getProject = (id, history) => async dispatch => {
-   try {
+  try {
     const res = await axios.get(`http://localhost:8080/api/project/${id}`);
     dispatch({
       type: GET_PROJECT,
@@ -30,4 +35,12 @@ export const getProject = (id, history) => async dispatch => {
   } catch (error) {
     history.push("/dashboard");
   }
+};
+
+export const deleteProject = id => async dispatch => {
+  await axios.delete(`http://localhost:8080/api/project/${id}`);
+  dispatch({
+    type: DELETE_PROJECT,
+    payload: id
+  });
 };
