c0d88d809c084ec9dc4a765c971686d60f6920d0
Dilruba Khan <rubatanisa@yahoo.ie>
2020-05-30 19:46:32 +0100

    Implement Update project task

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
-import Header from './components/layout/Header';
+import Header from './components/Layout/Header';
 import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter as Router, Route} from "react-router-dom";
 import AddProject from './components/Project/AddProject';
@@ -11,6 +11,7 @@ import store from "./store";
 import UpdateProject from "./components/Project/UpdateProject";
 import ProjectBoard from "./components/ProjectBoard/ProjectBoard";
 import AddProjectTask from "./components/ProjectBoard/ProjectTasks/AddProjectTask";
+import UpdateProjectTask from "./components/ProjectBoard/ProjectTasks/UpdateProjectTask";
 
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
           <Route exact path="/updateProject/:id" component={UpdateProject} />
           <Route exact path="/projectBoard/:id" component={ProjectBoard} />
           <Route exact path="/addProjectTask/:id" component={AddProjectTask} />
+          <Route exact path="/updateProjectTask/:backlog_id/:pt_id" component={UpdateProjectTask}/>
 
         </div>
       </Router>
--- src/components/ProjectBoard/ProjectTasks/ProjectTask.js
+++ src/components/ProjectBoard/ProjectTasks/ProjectTask.js
@@ -1,44 +1,156 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import classnames from "classnames";
+import { addProjectTask } from "../../../actions/backlogActions";
+import PropTypes from "prop-types";
 
-class ProjectTask extends Component {
+class AddProjectTask extends Component {
+  constructor(props) {
+    super(props);
+    const { id } = this.props.match.params;
+
+    this.state = {
+      summary: "",
+      acceptanceCriteria: "",
+      status: "",
+      priority: 0,
+      dueDate: "",
+      projectIdentifier: id,
+      errors: {}
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
+  }
+
+  // on change
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value });
+  }
+  //on submit
+  onSubmit(e) {
+    e.preventDefault();
+
+    const newTask = {
+      summary: this.state.summary,
+      acceptanceCriteria: this.state.acceptanceCriteria,
+      status: this.state.status,
+      priority: this.state.priority,
+      dueDate: this.state.dueDate
+    };
+
+    this.props.addProjectTask(
+      this.state.projectIdentifier,
+      newTask,
+      this.props.history
+    );
+  }
+
   render() {
-    const { project_task } = this.props;
-    let priorityString;
-    let priorityClass;
-
-    if (project_task.priority === 1) {
-      priorityClass = "bg-danger text-light";
-      priorityString = "HIGH";
-    }
-
-    if (project_task.priority === 2) {
-      priorityClass = "bg-warning text-light";
-      priorityString = "MEDIUM";
-    }
-
-    if (project_task.priority === 3) {
-      priorityClass = "bg-info text-light";
-      priorityString = "LOW";
-    }
-
+    const { id } = this.props.match.params;
+    const { errors } = this.state;
     return (
-      <div className="card mb-1 bg-light">
-        <div className={`card-header text-primary ${priorityClass}`}>
-          ID: {project_task.projectSequence} -- Priority: {priorityString}
-        </div>
-        <div className="card-body bg-light">
-          <h5 className="card-title">{project_task.summary}</h5>
-          <p className="card-text text-truncate ">
-            {project_task.acceptanceCriteria}
-          </p>
-          <a href="" className="btn btn-primary">
-            View / Update
-          </a>
+      <div className="add-PBI">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-8 m-auto">
+              <Link to={`/projectBoard/${id}`} className="btn btn-light">
+                Back to Project Board
+              </Link>
+              <h4 className="display-4 text-center">Add Project Task</h4>
+              <p className="lead text-center">Project Name + Project Code</p>
+              <form onSubmit={this.onSubmit}>
+                <div className="form-group">
+                  <input
+                    type="text"
+                    className={classnames("form-control form-control-lg", {
+                      "is-invalid": errors.summary
+                    })}
+                    name="summary"
+                    placeholder="Project Task summary"
+                    value={this.state.summary}
+                    onChange={this.onChange}
+                  />
+                  {errors.summary && (
+                    <div className="invalid-feedback">{errors.summary}</div>
+                  )}
+                </div>
+                <div className="form-group">
+                  <textarea
+                    className="form-control form-control-lg"
+                    placeholder="Acceptance Criteria"
+                    name="acceptanceCriteria"
+                    value={this.state.acceptanceCriteria}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <h6>Due Date</h6>
+                <div className="form-group">
+                  <input
+                    type="date"
+                    className="form-control form-control-lg"
+                    name="dueDate"
+                    value={this.state.dueDate}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div className="form-group">
+                  <select
+                    className="form-control form-control-lg"
+                    name="priority"
+                    value={this.state.priority}
+                    onChange={this.onChange}
+                  >
+                    <option value={0}>Select Priority</option>
+                    <option value={1}>High</option>
+                    <option value={2}>Medium</option>
+                    <option value={3}>Low</option>
+                  </select>
+                </div>
 
-          <button className="btn btn-danger ml-4">Delete</button>
+                <div className="form-group">
+                  <select
+                    className="form-control form-control-lg"
+                    name="status"
+                    value={this.state.status}
+                    onChange={this.onChange}
+                  >
+                    <option value="">Select Status</option>
+                    <option value="TO_DO">TO DO</option>
+                    <option value="IN_PROGRESS">IN PROGRESS</option>
+                    <option value="DONE">DONE</option>
+                  </select>
+                </div>
+
+                <input
+                  type="submit"
+                  className="btn btn-primary btn-block mt-4"
+                />
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     );
   }
 }
-export default ProjectTask;
+
+AddProjectTask.propTypes = {
+  addProjectTask: PropTypes.func.isRequired,
+  errors: PropTypes.object.isRequired
+};
+
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+
+export default connect(
+  mapStateToProps,
+  { addProjectTask }
+)(AddProjectTask);
